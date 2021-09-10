520160a96b09a56f35a610cc87c89995d93a019c
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-09-10 09:52:00 -0400

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,20 @@
 class PostsController < ApplicationController
-
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
 
   def post_params
-    params.permit(:category, :content, :title)
+    params.permit(:title, :category, :content)
   end
-
 end
